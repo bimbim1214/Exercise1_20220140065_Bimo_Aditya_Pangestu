f02aa12310a5b43010389e295807ff3b2d8f744f
bimbim1214 <bimoadityapangestu@gmail.com>
2024-04-01 21:10:55 +0700

    first commit

---
+++ scrip.js
@@ -0,0 +1,19 @@
+function showAlert(){
+    alert(Date(), tampil(), show())
+    Date()
+
+    let tanggal = new Date();
+    Document.getElementById("hasil").innerHTML = tanggal;
+    
+    function tampil(){
+        alert("Mobil keluaran terbaru")
+    }
+
+    function show(){
+        alert("Mobil jepang berkelas")
+    }
+
+    function show(){
+        alert("Mobil sport yang elegan")
+    }
+}
